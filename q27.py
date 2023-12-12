bf0940b64ee4ea795c3b7949e3cfd980ed1313cc
Akshay <akshayrk0319@gmail.com>
2023-12-12 22:38:26 +0530

    file sended

---
+++ q27.py
@@ -0,0 +1,7 @@
+'''27. Define a function that can convert a integer into a string and print it in console.'''
+def string(a):
+    return f"{str(a)}"
+a=int(input("enter a integer value: "))
+a=string(a)
+print(type(a))
+print(a+"ak")
